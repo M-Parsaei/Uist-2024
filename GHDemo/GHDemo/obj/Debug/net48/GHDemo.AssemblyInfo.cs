fabdc6c528ba2766cd11f909634bb7c218344cea
M-Parsaei <khodadostetdarm@gmail.com>
2024-01-27 19:34:59 -0700

    Made the initial UI using Human Ui, added gitignore

--- GHDemo/GHDemo/obj/Debug/net48/GHDemo.AssemblyInfo.cs
+++ GHDemo/GHDemo/obj/Debug/net48/GHDemo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of GHDemo")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+75d067f0c96b655c8a868873ea7e52b2c6249dbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("GHDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GHDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
